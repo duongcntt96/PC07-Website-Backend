1bf6b286f53f1c280299d82062935c6456a7f828
Dương <duong.ht96@gmail.com>
2022-09-27 10:24:43 +0700

    Fix requirement

--- PC07/settings.py
+++ PC07/settings.py
@@ -4,7 +4,8 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = ''
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = ''
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -19,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'django_filters',
     'corsheaders',
     'rest_framework',
@@ -118,6 +119,9 @@ USE_TZ = True
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
